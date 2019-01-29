dd3b92c3ac1457d6565f9bf2b4bec38f45023622
Hermes Waldemarin Neto <hermesneto@plusoft.com.br>
2019-01-29 21:29:48 -0200

    start scala migration

---
+++ src/main/scala/eurekaserver/Application.scala
@@ -0,0 +1,26 @@
+package eurekaserver
+
+import org.springframework.boot.{ApplicationRunner, SpringApplication}
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.config.server.EnableConfigServer
+import org.springframework.context.annotation.{Bean, ComponentScan}
+import org.springframework.web.client.RestTemplate
+
+@SpringBootApplication
+@ComponentScan(basePackages = Array("configserver"))
+@EnableEurekaServer
+class Application{
+
+  @Bean
+  def init(): ApplicationRunner = _ => {
+
+  }
+
+  @Bean
+  def restTemplate() : RestTemplate = new RestTemplate()
+}
+
+
+object Application extends App {
+  SpringApplication.run(classOf[Application], args: _*)
+}
